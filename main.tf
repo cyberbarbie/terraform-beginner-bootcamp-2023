382273b68cd871f562ae4ca25a87068c69759a9a
Tae'lur Alexis <taeluralexis@gmail.com>
2023-10-13 02:47:42 +0000

    #13 provision s3 resource with terraform iac

--- main.tf
+++ main.tf
@@ -1,19 +1,36 @@
+# we can only have on TF block 
 terraform {
   required_providers {
     random = {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.21.0"
+    }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
   # Configuration options
 }
 
+# specifying constraints for random string generator to follow AWS bucket naming rules 
 resource "random_string" "bucket_name" {
-  length = 16
+  length = 32
+  lower = true
+  upper = false
+  numeric = false
   special = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
